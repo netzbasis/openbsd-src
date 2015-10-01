a239c0963cdf51f9ce75d859fa8b7cabacc57852
Ben <programmer@netzbasis.de>
2015-10-01 08:02:59 +0200

    sync with openbsd-current

--- sys/arch/octeon/include/disklabel.h
+++ sys/arch/octeon/include/disklabel.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: disklabel.h,v 1.1 2010/09/20 06:32:30 syuu Exp $ */
+/* $OpenBSD: disklabel.h,v 1.3 2015/09/30 15:35:30 krw Exp $ */
 /* public domain */
 
 /*
@@ -6,6 +6,11 @@
  * of the OpenBSD partition.
  */
 
-#define	LABELSECTOR	1
-#define	LABELOFFSET	0
-#define	MAXPARTITIONS	16
+#ifndef _MACHINE_DISKLABEL_H_
+#define _MACHINE_DISKLABEL_H_
+
+#define	LABELSECTOR	1	/* sector containing label */
+#define	LABELOFFSET	0	/* offset of label in sector */
+#define	MAXPARTITIONS	16	/* number of partitions */
+
+#endif /* _MACHINE_DISKLABEL_H_ */
