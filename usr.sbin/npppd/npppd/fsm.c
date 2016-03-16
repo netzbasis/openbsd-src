145a811415c902f1ac0a8efb292448ea33f65c01
Ben <programmer@netzbasis.de>
2016-03-16 19:49:13 +0100

    sync with openbsd-current

--- usr.sbin/npppd/npppd/fsm.c
+++ usr.sbin/npppd/npppd/fsm.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: fsm.c,v 1.6 2014/11/18 20:54:28 krw Exp $ */
+/*	$OpenBSD: fsm.c,v 1.8 2016/03/16 04:58:34 yasuoka Exp $ */
 
 /**@file
  * This file was adapted from NetBSD:/usr/src/usr.sbin/pppd/pppd/fsm.c
@@ -463,7 +463,7 @@ fsm_rconfreq(f, id, inp, len)
 	f->nakloops = 0;
 
     } else {
-	/* we sent CONFACK or CONFREJ */
+	/* we sent CONFNAK or CONFREJ */
 	if (f->state != ACKRCVD)
 	    f->state = REQSENT;
 	if( code == CONFNAK )
@@ -646,6 +646,7 @@ fsm_rtermack(f)
 	if (f->callbacks->down)
 	    (*f->callbacks->down)(f);	/* Inform upper layers */
 	fsm_sconfreq(f, 0);
+	f->state = REQSENT;
 	break;
     }
 }
