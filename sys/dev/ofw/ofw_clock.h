4884a5253eae18e1f95dae738fc028aa4eef5a88
Ben <programmer@netzbasis.de>
2016-08-22 08:08:46 +0200

    sync with openbsd-current

--- sys/dev/ofw/ofw_clock.h
+++ sys/dev/ofw/ofw_clock.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ofw_regulator.h,v 1.2 2016/08/21 14:41:51 kettenis Exp $	*/
+/*	$OpenBSD: ofw_clock.h,v 1.1 2016/08/21 21:38:05 kettenis Exp $	*/
 /*
  * Copyright (c) 2016 Mark Kettenis
  *
@@ -15,10 +15,25 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-#ifndef _DEV_OFW_REGULATOR_H_
-#define _DEV_OFW_REGULATOR_H_
+#ifndef _DEV_OFW_CLOCK_H_
+#define _DEV_OFW_CLOCK_H_
 
-int	regulator_enable(uint32_t);
-int	regulator_disable(uint32_t);
+struct clock_device {
+	int	cd_node;
+	void	*cd_cookie;
+	uint32_t (*cd_get_frequency)(void *, uint32_t *);
+	void	(*cd_enable)(void *, uint32_t *, int);
 
-#endif /* _DEV_OFW_REGULATOR_H_ */
+	LIST_ENTRY(clock_device) cd_list;
+	uint32_t cd_phandle;
+	uint32_t cd_cells;
+};
+
+void	clock_register(struct clock_device *);
+
+uint32_t clock_get_frequency(int, const char *);
+uint32_t clock_get_frequency_idx(int, int);
+void	clock_enable(int, const char *);
+void	clock_enable_idx(int, int);
+
+#endif /* _DEV_OFW_CLOCK_H_ */
