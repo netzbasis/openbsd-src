976daad23665f7dcc5b2b321bbb8666883dcc712
Ben <programmer@netzbasis.de>
2017-03-12 08:04:53 +0100

    sync with openbsd-current

--- sbin/ifconfig/ifconfig.c
+++ sbin/ifconfig/ifconfig.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ifconfig.c,v 1.337 2017/01/21 08:31:44 krw Exp $	*/
+/*	$OpenBSD: ifconfig.c,v 1.339 2017/03/12 03:18:57 stsp Exp $	*/
 /*	$NetBSD: ifconfig.c,v 1.40 1997/10/01 02:19:43 enami Exp $	*/
 
 /*
@@ -2375,11 +2375,12 @@ ieee80211_printnode(struct ieee80211_nodereq *nr)
 	if (nr->nr_capinfo) {
 		printb_status(nr->nr_capinfo, IEEE80211_CAPINFO_BITS);
 		if (nr->nr_capinfo & IEEE80211_CAPINFO_PRIVACY) {
-			if (nr->nr_rsnciphers & IEEE80211_WPA_CIPHER_CCMP)
-				fputs(",wpa2", stdout);
-			else if (nr->nr_rsnciphers & IEEE80211_WPA_CIPHER_TKIP)
-				fputs(",wpa1", stdout);
-			else
+			if (nr->nr_rsnprotos) {
+				if (nr->nr_rsnprotos & IEEE80211_WPA_PROTO_WPA2)
+					fputs(",wpa2", stdout);
+				if (nr->nr_rsnprotos & IEEE80211_WPA_PROTO_WPA1)
+					fputs(",wpa1", stdout);
+			} else
 				fputs(",wep", stdout);
 
 			if (nr->nr_rsnakms & IEEE80211_WPA_AKM_8021X ||
