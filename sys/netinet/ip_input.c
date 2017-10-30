01d856fde32e81ef5024444e33b89b46e8126d59
Ben <programmer@netzbasis.de>
2017-10-30 08:25:31 +0100

    sync with openbsd-current

--- sys/netinet/ip_input.c
+++ sys/netinet/ip_input.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ip_input.c,v 1.324 2017/10/26 15:13:40 mpi Exp $	*/
+/*	$OpenBSD: ip_input.c,v 1.326 2017/10/29 14:58:39 florian Exp $	*/
 /*	$NetBSD: ip_input.c,v 1.30 1996/03/16 23:53:58 christos Exp $	*/
 
 /*
