ab596b1808af6a90a267484ec83eba3e179fed84
Ben <programmer@netzbasis.de>
2018-08-04 08:20:17 +0200

    sync with openbsd-current

--- usr.sbin/bgpd/rde.c
+++ usr.sbin/bgpd/rde.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: rde.c,v 1.406 2018/08/02 12:49:00 claudio Exp $ */
+/*	$OpenBSD: rde.c,v 1.408 2018/08/03 16:31:22 claudio Exp $ */
 
 /*
  * Copyright (c) 2003, 2004 Henning Brauer <henning@openbsd.org>
@@ -90,6 +90,7 @@ void		 rde_rib_free(struct rib_desc *);
 
 int		 rde_rdomain_import(struct rde_aspath *, struct rdomain *);
 void		 rde_reload_done(void);
+static void	 rde_softreconfig_done(void *);
 static void	 rde_softreconfig_out(struct rib_entry *, void *);
 static void	 rde_softreconfig_in(struct rib_entry *, void *);
 static void	 rde_softreconfig_unload_peer(struct rib_entry *, void *);
@@ -124,7 +125,7 @@ struct bgpd_config	*conf, *nconf;
 time_t			 reloadtime;
 struct rde_peer_head	 peerlist;
 struct rde_peer		*peerself;
-struct prefixset_head	*prefixsets_tmp;
+struct prefixset_head	*prefixsets_tmp, *prefixsets_old;
 struct filter_head	*out_rules, *out_rules_tmp;
 struct rdomain_head	*rdomains_l, *newdomains;
 struct imsgbuf		*ibuf_se;
@@ -990,7 +991,7 @@ rde_update_dispatch(struct imsg *imsg)
 	    imsg->hdr.len - IMSG_HEADER_SIZE - 4 - withdrawn_len - attrpath_len;
 	bzero(&mpa, sizeof(mpa));
 
-	rde_filterstate_prep(&state, NULL, NULL);
+	rde_filterstate_prep(&state, NULL, NULL, 0);
 	if (attrpath_len != 0) { /* 0 = no NLRI information in this message */
 		/* parse path attributes */
 		while (len > 0) {
@@ -1305,7 +1306,8 @@ rde_update_update(struct rde_peer *peer, struct filterstate *in,
 	for (i = RIB_LOC_START; i < rib_size; i++) {
 		if (*ribs[i].name == '\0')
 			break;
-		rde_filterstate_prep(&state, &in->aspath, in->nexthop);
+		rde_filterstate_prep(&state, &in->aspath, in->nexthop,
+		    in->nhflags);
 		/* input filter */
 		action = rde_filter(ribs[i].in_rules, peer, p, &state);
 
@@ -2099,7 +2101,8 @@ rde_dump_filterout(struct rde_peer *peer, struct prefix *p,
 	if (up_test_update(peer, p) != 1)
 		return;
 
-	rde_filterstate_prep(&state, prefix_aspath(p), prefix_nexthop(p));
+	rde_filterstate_prep(&state, prefix_aspath(p), prefix_nexthop(p),
+	    prefix_nhflags(p));
 	a = rde_filter(out_rules, peer, p, &state);
 
 	if (a == ACTION_ALLOW)
@@ -2420,9 +2423,9 @@ rde_send_kroute(struct rib *rib, struct prefix *new, struct prefix *old)
 	bzero(&kr, sizeof(kr));
 	memcpy(&kr.prefix, &addr, sizeof(kr.prefix));
 	kr.prefixlen = p->re->prefix->prefixlen;
-	if (asp->flags & F_NEXTHOP_REJECT)
+	if (prefix_nhflags(p) == NEXTHOP_REJECT)
 		kr.flags |= F_REJECT;
-	if (asp->flags & F_NEXTHOP_BLACKHOLE)
+	if (prefix_nhflags(p) == NEXTHOP_BLACKHOLE)
 		kr.flags |= F_BLACKHOLE;
 	if (type == IMSG_KROUTE_CHANGE)
 		memcpy(&kr.nexthop, &prefix_nexthop(p)->true_nexthop,
@@ -2697,7 +2700,7 @@ rde_reload_done(void)
 	struct rde_peer		*peer;
 	struct filter_head	*fh;
 	u_int16_t		 rid;
-	struct prefixset_head	*prefixsets_old;
+	int			 reload = 0;
 
 	/* first merge the main config */
 	if ((nconf->flags & BGPD_FLAG_NO_EVALUATE)
@@ -2801,10 +2804,11 @@ rde_reload_done(void)
 			log_debug("in filter change: reloading RIB %s",
 			    ribs[rid].name);
 			ribs[rid].state = RECONF_RELOAD;
-			/* FALLTHROUGH */
+			reload++;
+			break;
 		case RECONF_REINIT:
-			rib_dump(&ribs[RIB_ADJ_IN].rib, rde_softreconfig_in,
-			    &ribs[rid], AID_UNSPEC);
+			ribs[rid].state = RECONF_RELOAD;
+			reload++;
 			break;
 		case RECONF_RELOAD:
 			log_warnx("Bad rib reload state");
@@ -2812,22 +2816,44 @@ rde_reload_done(void)
 		case RECONF_NONE:
 			break;
 		}
+		filterlist_free(ribs[rid].in_rules_tmp);
+		ribs[rid].in_rules_tmp = NULL;
 	}
+
+	if (reload > 0)
+		rib_dump(&ribs[RIB_ADJ_IN].rib, rde_softreconfig_in, NULL,
+		    AID_UNSPEC);
+
+	/* now do the Adj-RIB-Out sync */
+	for (rid = 0; rid < rib_size; rid++)
+		ribs[rid].state = RECONF_NONE;
+
 	LIST_FOREACH(peer, &peerlist, peer_l) {
 		if (peer->reconf_out)
-			rib_dump(peer->rib, rde_softreconfig_out,
-			    peer, AID_UNSPEC);
+			ribs[peer->rib->id].state = RECONF_RELOAD;
 		else if (peer->reconf_rib)
 			/* dump the full table to neighbors that changed rib */
 			peer_dump(peer->conf.id, AID_UNSPEC);
 	}
+
+	for (rid = 0; rid < rib_size; rid++)
+		if (ribs[rid].state == RECONF_RELOAD)
+			rib_dump(&ribs[rid].rib, rde_softreconfig_out, NULL,
+			    AID_UNSPEC);
+
+	rde_softreconfig_done(NULL);
+}
+
+static void
+rde_softreconfig_done(void *arg)
+{
+	u_int16_t		 rid;
+
 	filterlist_free(out_rules_tmp);
 	out_rules_tmp = NULL;
 	for (rid = 0; rid < rib_size; rid++) {
 		if (*ribs[rid].name == '\0')
 			continue;
-		filterlist_free(ribs[rid].in_rules_tmp);
-		ribs[rid].in_rules_tmp = NULL;
 		ribs[rid].state = RECONF_NONE;
 	}
 
@@ -2840,16 +2866,17 @@ rde_reload_done(void)
 }
 
 static void
-rde_softreconfig_in(struct rib_entry *re, void *ptr)
+rde_softreconfig_in(struct rib_entry *re, void *bula)
 {
 	struct filterstate	 state;
-	struct rib_desc		*rib = ptr;
+	struct rib_desc		*rib;
 	struct prefix		*p;
 	struct pt_entry		*pt;
 	struct rde_peer		*peer;
 	struct rde_aspath	*asp;
 	enum filter_actions	 action;
 	struct bgpd_addr	 addr;
+	u_int16_t		 i;
 
 	pt = re->prefix;
 	pt_getaddr(pt, &addr);
@@ -2857,37 +2884,40 @@ rde_softreconfig_in(struct rib_entry *re, void *ptr)
 		asp = prefix_aspath(p);
 		peer = asp->peer;
 
-		rde_filterstate_prep(&state, asp, prefix_nexthop(p));
-		action = rde_filter(rib->in_rules, peer, p, &state);
+		for (i = RIB_LOC_START; i < rib_size; i++) {
+			rib = &ribs[i];
+			if (rib->state != RECONF_RELOAD)
+				continue;
+			if (*rib->name == '\0')
+				break;
 
-		if (action == ACTION_ALLOW) {
-			/* update Local-RIB */
-			path_update(&rib->rib, peer, &state, &addr,
-			    pt->prefixlen, 0);
-		} else if (action == ACTION_DENY) {
-			/* remove from Local-RIB */
-			prefix_remove(&rib->rib, peer, &addr, pt->prefixlen, 0);
+			rde_filterstate_prep(&state, asp, prefix_nexthop(p),
+			    prefix_nhflags(p));
+			action = rde_filter(rib->in_rules, peer, p, &state);
+
+			if (action == ACTION_ALLOW) {
+				/* update Local-RIB */
+				path_update(&rib->rib, peer, &state, &addr,
+				    pt->prefixlen, 0);
+			} else if (action == ACTION_DENY) {
+				/* remove from Local-RIB */
+				prefix_remove(&rib->rib, peer, &addr,
+				    pt->prefixlen, 0);
+			}
+
+			rde_filterstate_clean(&state);
 		}
-
-		rde_filterstate_clean(&state);
 	}
 }
 
 static void
-rde_softreconfig_out(struct rib_entry *re, void *ptr)
+rde_softreconfig_out_peer(struct rib_entry *re, struct rde_peer *peer)
 {
 	struct filterstate	 ostate, nstate;
+	struct bgpd_addr	 addr;
 	struct prefix		*p = re->active;
 	struct pt_entry		*pt;
-	struct rde_peer		*peer = ptr;
 	enum filter_actions	 oa, na;
-	struct bgpd_addr	 addr;
-
-	if (peer->conf.id == 0)
-		fatalx("King Bula troubled by bad peer");
-
-	if (p == NULL)
-		return;
 
 	pt = re->prefix;
 	pt_getaddr(pt, &addr);
@@ -2895,8 +2925,10 @@ rde_softreconfig_out(struct rib_entry *re, void *ptr)
 	if (up_test_update(peer, p) != 1)
 		return;
 
-	rde_filterstate_prep(&ostate, prefix_aspath(p), prefix_nexthop(p));
-	rde_filterstate_prep(&nstate, prefix_aspath(p), prefix_nexthop(p));
+	rde_filterstate_prep(&ostate, prefix_aspath(p), prefix_nexthop(p),
+	    prefix_nhflags(p));
+	rde_filterstate_prep(&nstate, prefix_aspath(p), prefix_nexthop(p),
+	    prefix_nhflags(p));
 	oa = rde_filter(out_rules_tmp, peer, p, &ostate);
 	na = rde_filter(out_rules, peer, p, &nstate);
 
@@ -2910,22 +2942,29 @@ rde_softreconfig_out(struct rib_entry *re, void *ptr)
 		/* send withdraw */
 		up_generate(peer, NULL, &addr, pt->prefixlen);
 	} else if (oa == ACTION_ALLOW && na == ACTION_ALLOW) {
-		/* XXX update nexthop for now, ugly but will go away */
-		nexthop_put(nstate.aspath.nexthop);
-		nstate.aspath.nexthop = nexthop_ref(nstate.nexthop);
-		nstate.aspath.flags = (nstate.aspath.flags & ~F_NEXTHOP_MASK) |
-		    (nstate.nhflags & F_NEXTHOP_MASK);
-		nexthop_put(ostate.aspath.nexthop);
-		ostate.aspath.nexthop = nexthop_ref(ostate.nexthop);
-		ostate.aspath.flags = (ostate.aspath.flags & ~F_NEXTHOP_MASK) |
-		    (ostate.nhflags & F_NEXTHOP_MASK);
-		/* send update if path attributes changed */
-		if (path_compare(&nstate.aspath, &ostate.aspath) != 0)
+		/* send update if anything changed */
+		if (nstate.nhflags != ostate.nhflags ||
+		    nstate.nexthop != ostate.nexthop ||
+		    path_compare(&nstate.aspath, &ostate.aspath) != 0)
 			up_generate(peer, &nstate, &addr, pt->prefixlen);
 	}
 
 	rde_filterstate_clean(&ostate);
 	rde_filterstate_clean(&nstate);
+}
+
+static void
+rde_softreconfig_out(struct rib_entry *re, void *bula)
+{
+	struct rde_peer		*peer;
+
+	if (re->active == NULL)
+		return;
+
+	LIST_FOREACH(peer, &peerlist, peer_l) {
+		if (peer->rib == re_rib(re) && peer->reconf_out)
+			rde_softreconfig_out_peer(re, peer);
+	}
 }
 
 static void
@@ -2944,7 +2983,8 @@ rde_softreconfig_unload_peer(struct rib_entry *re, void *ptr)
 	if (up_test_update(peer, p) != 1)
 		return;
 
-	rde_filterstate_prep(&ostate, prefix_aspath(p), prefix_nexthop(p));
+	rde_filterstate_prep(&ostate, prefix_aspath(p), prefix_nexthop(p),
+	    prefix_nhflags(p));
 	if (rde_filter(out_rules_tmp, peer, p, &ostate) != ACTION_DENY) {
 		/* send withdraw */
 		up_generate(peer, NULL, &addr, pt->prefixlen);
@@ -3282,12 +3322,13 @@ peer_dump(u_int32_t id, u_int8_t aid)
 		return;
 	}
 
-	if (peer->conf.export_type == EXPORT_NONE)
+	if (peer->conf.export_type == EXPORT_NONE) {
 		/* nothing */;
-	else if (peer->conf.export_type == EXPORT_DEFAULT_ROUTE)
+	} else if (peer->conf.export_type == EXPORT_DEFAULT_ROUTE) {
 		up_generate_default(out_rules, peer, aid);
-	else
+	} else {
 		rib_dump(peer->rib, rde_up_dump_upcall, peer, aid);
+	}
 	if (peer->capa.grestart.restart)
 		up_generate_marker(peer, aid);
 }
@@ -3419,7 +3460,7 @@ network_add(struct network_config *nc, int flagstatic)
 	}
 	if (!flagstatic)
 		asp->flags |= F_ANN_DYNAMIC;
-	rde_filterstate_prep(&state, asp, NULL); /* nexthop is not set */
+	rde_filterstate_prep(&state, asp, NULL, 0); /* nexthop is not set */
 	rde_apply_set(&nc->attrset, &state, nc->prefix.aid, peerself, peerself);
 	if (vpnset)
 		rde_apply_set(vpnset, &state, nc->prefix.aid, peerself,
