4803cd7db0e59586d17a892ff6c6c6e317da78d7
Ben <programmer@netzbasis.de>
2019-12-01 08:26:40 +0100

    sync with openbsd-current

--- sbin/unwind/resolver.c
+++ sbin/unwind/resolver.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: resolver.c,v 1.84 2019/11/29 15:22:02 florian Exp $	*/
+/*	$OpenBSD: resolver.c,v 1.86 2019/11/30 16:14:03 otto Exp $	*/
 
 /*
  * Copyright (c) 2018 Florian Obser <florian@openbsd.org>
@@ -756,12 +756,18 @@ try_next_resolver(struct running_query *rq)
 	memcpy(query_imsg, rq->query_imsg, sizeof(*query_imsg));
 	clock_gettime(CLOCK_MONOTONIC, &query_imsg->tp);
 
+	ms = histogram_median(res->latest_histogram);
+	if (ms == INT64_MAX)
+		ms = 2000;
 	if (res->type == resolver_conf->res_pref.types[0])
-		tv.tv_usec = 1000 * (PREF_RESOLVER_MEDIAN_SKEW +
-		    histogram_median(res->latest_histogram));
+		tv.tv_usec = 1000 * (PREF_RESOLVER_MEDIAN_SKEW + ms);
 	else
-		tv.tv_usec = 1000 * histogram_median(res->latest_histogram);
+		tv.tv_usec = 1000 * ms;
 
+	while (tv.tv_usec >= 1000000) {
+		tv.tv_sec++;
+		tv.tv_usec -= 1000000;
+	}
 	evtimer_add(&rq->timer_ev, &tv);
 
 	if (resolve(res, query_imsg->qname, query_imsg->t,
@@ -1322,6 +1328,9 @@ check_resolver(struct uw_resolver *resolver_to_check)
 {
 	struct uw_resolver		*res;
 
+	if (resolver_to_check == NULL)
+		return;
+
 	if (resolver_to_check->check_running) {
 		log_debug("%s: already checking: %s", __func__,
 		    uw_resolver_type_str[resolver_to_check->type]);
@@ -1330,7 +1339,7 @@ check_resolver(struct uw_resolver *resolver_to_check)
 
 	log_debug("%s: create_resolver", __func__);
 	if ((res = create_resolver(resolver_to_check->type, 0)) == NULL)
-		fatal("%s", __func__);
+		return;
 
 	resolver_ref(resolver_to_check);
 
@@ -1355,7 +1364,7 @@ check_resolver(struct uw_resolver *resolver_to_check)
 
 	log_debug("%s: create_resolver for oppdot", __func__);
 	if ((res = create_resolver(resolver_to_check->type, 1)) == NULL)
-		fatal("%s", __func__);
+		return;
 
 	resolver_ref(resolver_to_check);
 
